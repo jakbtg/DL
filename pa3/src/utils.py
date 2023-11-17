b400492d1e8c8e4179873e4f8e29631abc46ac48
Giacomo Grandi <jak.grandi@gmail.com>
2023-11-17 12:21:42 +0100

    cleaning datasets

--- pa3/src/utils.py
+++ pa3/src/utils.py
@@ -28,7 +28,33 @@ def build_datasets(force=False):
             writer.writerows(data[20000:])
 
 
+def clean_data():
+    for file in os.listdir('../data'):
+        if file.endswith('.csv') and file != 'original.csv':
+            with open('../data/' + file, 'r') as f:
+                reader = csv.reader(f)
+                data = list(reader)
+                # Check if it is already cleaned by checking number of columns, if it is 2, then it is already cleaned
+                if len(data[0]) == 2:
+                    print(f'{file} is already cleaned')
+                    continue
+                # Convert the labels from 0,4 to 0,1
+                for row in data:
+                    if row[0] == '0':
+                        row[0] = '0'
+                    elif row[0] == '4':
+                        row[0] = '1'
+                # Remove all columns except the first and last
+                for row in data:
+                    del row[1:-1]
+                # Write the cleaned data to the file
+                with open('../data/' + file, 'w') as f:
+                    writer = csv.writer(f)
+                    writer.writerows(data)
+
+
+
 if __name__ == '__main__':
     build_datasets()
-    
+    clean_data()
 
